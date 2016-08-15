5378314982c504f62a64ef7fa6c524b2b179b0af
linqiaonihao <824928207@qq.com>
2016-08-15 10:42:10 +0800

    java

--- JDK核心API/java.io/src/FileDemo2.java
+++ JDK核心API/java.io/src/FileDemo2.java
@@ -5,8 +5,8 @@ public class FileDemo2 {
 	public static void main(String[] args) {
         int data_arr[] = {12, 32, 43, 45, 1, 5};
         try {
-            RandomAccessFile randf=new RandomAccessFile("temp.dat","rw");
-            for(int i = 0; i < data_arr.length; i++){
+            RandomAccessFile randf = new RandomAccessFile("temp.dat","rw");
+            for (int i = 0; i < data_arr.length; i++){
                 randf.writeInt(data_arr[i]);
             }
             for(int i = data_arr.length-1 ; i >= 0; i--){
