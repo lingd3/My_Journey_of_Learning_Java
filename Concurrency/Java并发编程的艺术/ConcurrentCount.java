b9c5f878a8aed2251d7022222685a59aced4daca
lingd3 <824928207@qq.com>
2017-08-28 22:35:45 +0800

    concurrent

---
+++ Concurrency/Java并发编程的艺术/ConcurrentCount.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ConcurrentCount {
+        
+    private AtomicInteger atomicI = new AtomicInteger(0);
+    
+    private int i = 0;
+    
+    /** 
+     * 使用CAS实现线程安全计数器 
+     */
+    private void safeCount() {
+        for (;;) {
+            int i = atomicI.get();
+            boolean suc = atomicI.compareAndSet(i, ++i);
+            if (suc) {
+                break;
+            }
+        }
+    }
+    
+    /**
+     * 非线程安全计数器
+     */
+    private void count() {
+        i++;
+    }
+    
+    public static void main(String[] args) {
+        final ConcurrentCount cas = new ConcurrentCount();
+        List<Thread> ts = new ArrayList<Thread>(600);
+        long start = System.currentTimeMillis();
+        for (int j = 0; j < 100; j++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        as.count();
+                        cas.safeCount();
+                    }
+                }
+            });
+            ts.add(t);
+        }
+        for (Thread t : ts) {
+            t.start();
+        }
+        // 等待所有线程执行完成
+        for (Thread t : ts) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(cas.i);
+        System.out.println(cas.atomicI.get());
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+}
