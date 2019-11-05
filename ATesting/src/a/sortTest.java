9666c5e4514f0579f8bddafeec3b2b30e80a000b
lingd3 <824928207@qq.com>
2019-11-05 15:23:31 +0800

    zookeeper

--- ATesting/src/a/sortTest.java
+++ ATesting/src/a/sortTest.java
@@ -11,18 +11,18 @@ import javax.management.Query;
 public class sortTest {
 	
 	public static void main(String[] args) {
-//		int[] q = {123,23,24,234,242,4,12,43,57,7867564};
-//		int len = q.length;
-//		for (int i = 0; i < len; i++) {
-//			for (int j = len-1; j > i; j--) {
-//				if (q[j] < q[j-1]) {
-//					int temp = q[j];
-//					q[j] = q[j-1];
-//					q[j-1] = temp;
-//				}
-//			}
-//		}
-//		for (int i = 0; i < len; i++) System.out.print(q[i]+" ");
+		int[] q = {123,23,24,234,242,4,12,43,57,7867564};
+		int len = q.length;
+		for (int i = 0; i < len; i++) {
+			for (int j = len-1; j > i; j--) {
+				if (q[j] < q[j-1]) {
+					int temp = q[j];
+					q[j] = q[j-1];
+					q[j-1] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < len; i++) System.out.print(q[i]+" ");
 		
 		int[] a = {12,3,4,56,7,8,987,4};
 		System.out.println(Arrays.toString(a));
